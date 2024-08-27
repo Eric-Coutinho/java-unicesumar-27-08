79d1ec5af8d23124b736e0e0c7a1d996ab484fcf
Eric-Coutinho <eric.coutinhop@hotmail.com>
2024-08-27 20:29:57 -0300

    exercises 2 to 4

---
+++ Capitulo3/src/exercicio/desafio3.java
@@ -0,0 +1,15 @@
+package exercicio;
+
+public class desafio3 {
+
+	public static void main(String[] args) {
+		int idade = 10;
+		
+		while (idade < 30) {
+			System.out.println("idade: " + idade);
+			idade++;
+		}
+		System.out.println("acabo");
+	}
+
+}
