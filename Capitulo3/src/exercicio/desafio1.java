71ca91bc23ea02f3318aafef081485be814faf07
Eric-Coutinho <eric.coutinhop@hotmail.com>
2024-08-27 20:05:54 -0300

    initializing repository, exercises 1 and 2

---
+++ Capitulo3/src/exercicio/desafio1.java
@@ -0,0 +1,13 @@
+package exercicio;
+
+public class desafio1 {
+
+	public static void main(String[] args) {
+		int valor = 18;
+		
+		if (valor < 18) {
+			System.out.println("ahahahahahhaha menor q 18 ahahahhhhahahahha");
+		}
+		System.out.println("acabo");
+	}
+}
---
+++ Capitulo3/src/exercicio/desafio2.java
@@ -0,0 +1,20 @@
+package exercicio;
+
+public class desafio2 {
+
+	public static void main(String[] args) {
+		int idade = 18;
+		
+		if (idade < 0) {
+			System.out.println("idade negativa n pode");
+		}	
+		else if (idade < 18) {
+			System.out.println("crianca ainda kkk");
+		}
+		else if (idade >= 18) {
+			System.out.println("ja ta podendo beber");
+		}
+		
+		System.out.println("acabo");
+	}
+}
